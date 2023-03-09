6ec6e7642a657ddf4e00dd73db7d81d809cdcc42
Vairamuthu <mvairamuthu2003@gmail.com>
2023-03-09 15:37:04 +0530

    updated the config files

--- next.config.js
+++ next.config.js
@@ -24,6 +24,14 @@ const nextConfig = {
   },
 };
 
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
 module.exports = nextConfig;
 module.exports = {
   typescript: {
