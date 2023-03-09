252d4900181b0141dfce331bf9b5aa033841b4b3
Vairamuthu <mvairamuthu2003@gmail.com>
2023-03-09 23:28:25 +0530

    updated the default rpc for the solona

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -1,1 +1,5 @@
-export const staticAddresses = ["CCZz1UAKw7o5ftDYtYPaR5oX4ZvC3QmsGNeCJeM3FMCP", "FQPxZebhpTqTCTBW8cHjoYgbPZVbMPZGJ5pNqE3GnGPo", "DMqD9QHpJuxVbr46A3hoHeJgZnkaK1C7TQfag8VVMvzz"];
+export default staticAddresses = [
+  "CCZz1UAKw7o5ftDYtYPaR5oX4ZvC3QmsGNeCJeM3FMCP",
+  "FQPxZebhpTqTCTBW8cHjoYgbPZVbMPZGJ5pNqE3GnGPo",
+  "DMqD9QHpJuxVbr46A3hoHeJgZnkaK1C7TQfag8VVMvzz",
+];
