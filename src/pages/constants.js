9ca717e12e1d983426efcd33e0e23853cf2622d5
Vairamuthu <mvairamuthu2003@gmail.com>
2023-03-09 23:33:58 +0530

    updated the static addresses

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -1,5 +1,7 @@
-export default staticAddresses = [
+const staticAddresses = [
   "CCZz1UAKw7o5ftDYtYPaR5oX4ZvC3QmsGNeCJeM3FMCP",
   "FQPxZebhpTqTCTBW8cHjoYgbPZVbMPZGJ5pNqE3GnGPo",
   "DMqD9QHpJuxVbr46A3hoHeJgZnkaK1C7TQfag8VVMvzz",
 ];
+
+export default staticAddresses;
