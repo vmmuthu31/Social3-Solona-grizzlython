cba63642e2926523bb087dcba729ab3386ab0bb8
Vairamuthu <mvairamuthu2003@gmail.com>
2023-03-09 23:37:45 +0530

    updated the static addresses

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,5 @@
-const staticAddresses = [
+export const staticAddresses = [
   "CCZz1UAKw7o5ftDYtYPaR5oX4ZvC3QmsGNeCJeM3FMCP",
   "FQPxZebhpTqTCTBW8cHjoYgbPZVbMPZGJ5pNqE3GnGPo",
   "DMqD9QHpJuxVbr46A3hoHeJgZnkaK1C7TQfag8VVMvzz",
 ];
-
-export default staticAddresses;
