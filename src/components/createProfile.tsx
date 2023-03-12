056a8fc009d90c09b90a80bc232b23d4678c42bb
Vairamuthu <mvairamuthu2003@gmail.com>
2023-03-12 23:32:27 +0530

    updated

--- src/components/createProfile.tsx
+++ src/components/createProfile.tsx
@@ -12,114 +12,115 @@ type Namespace = "Professional" | "Personal" | "Gaming" | "Degen";
 interface Props {
   sdk: SDK;
   myState: string | undefined;
-
 }
 
-export const handleCreateProfile = async (
-  userPDA: PublicKey,
-  namespace: Namespace,
-  user: PublicKey,
-  sdk: SDK
-) => {
-  if (!userPDA) return;
-  const program = await sdk.profile.create(userPDA, namespace, user);
-  await program.instructionMethodBuilder.rpc();
-};
+interface UserProfile {
+  name: string;
+  bio: string;
+  username: string;
+  avatar: string;
+}
 
-const CreateProfile = ({sdk}: Props) => {
+const CreateProfile = ({ sdk }: Props) => {
   const wallet = useWallet();
   const userPublicKey = wallet.publicKey as PublicKey;
-  const [usersList, setUsersList] = useState([]);
-  const [name, SetName] = useState('')
-  const [bio, SetBio] = useState('')
-  const [username, SetUsername] = useState('')
-  const [avatar, SetAvatar] = useState('')
-  const [selectedUserOption, setSelectedUserOption] = useState("");
+  const [usersList, setUsersList] = useState<string[]>([]);
+  const [userProfile, setUserProfile] = useState<UserProfile>({
+    name: '',
+    bio: '',
+    username: '',
+    avatar: '',
+  });
+  const [selectedUserOption, setSelectedUserOption] = useState('');
+ 
   const [metadataUri, setMetadataUri] = useState('');
+
   const { create, profilePDA, error, loading } = useCreateProfile(sdk);
-  const Profile = async (name:any, bio: String, username:String, avatar: String) => {
-    if (usersList.length > 0) return;
-    try {
-      const metadata_id = uuidv4();
-      const ipfsResult = await uploadToIPFS({
-        metadata_id,
-        name: name,
-        bio: bio,
-        username: username,
-        avatar: avatar,
-      });
-      const profilelink = `https://${ipfsResult}.ipfs.dweb.link/${metadata_id}.json`
-      console.log(profilelink)
-      setMetadataUri(profilelink)
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
-  const selectedNamespaceOption ="Professional"
+
   useEffect(() => {
     if (!wallet.connected) return;
+
     const init = async () => {
       const users = await sdk?.user.getUserAccountsByUser(userPublicKey) as any;
-      const usersList = users?.map((user: any) => user.publicKey.toBase58());
+      const usersList = users?.map((user: any) => user.publicKey.toBase58()) || [];
       setUsersList(usersList);
+
       if (usersList.length > 0) {
         setSelectedUserOption(usersList[0]);
       }
     };
+
     init();
   }, [wallet.connected]);
+
+  const handleUserProfileChange = (field: keyof UserProfile, value: string) => {
+    setUserProfile((prevState) => ({
+      ...prevState,
+      [field]: value,
+    }));
+  };
+
+  const handleCreateProfile = async () => {
+    // if (usersList.length > 0) return;
+
+    try {
+      const metadata_id = uuidv4();
+      const ipfsResult = await uploadToIPFS({
+        metadata_id,
+        ...userProfile,
+      });
+      const profilelink = `https://${ipfsResult}.ipfs.dweb.link/${metadata_id}.json`;
+      setMetadataUri(profilelink);
+      console.log(profilelink);
+    } catch (error) {
+      console.log('error', error);
+    }
+   const selectedNamespaceOption= "Professional"
+
+    setTimeout(() => {
+      create(metadataUri, selectedNamespaceOption, new PublicKey(selectedUserOption), userPublicKey);
+    }, 2000);
+  };
   
 
   return (
-    <div className={`${styles.minimize}`}>
-     
-      <div className={`${styles.field}`}>
-        <label className={`${styles.label}`}>Enter Name:</label>
-        <input
-          type="text"
-          value={name}
-          onChange={(event) => SetName(event.target.value)}
-          className={`${styles.input}`}
-        />
-        <label className={`${styles.label}`}>Enter Username:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(event) => SetUsername(event.target.value)}
-          className={`${styles.input}`}
-        />
-        <label className={`${styles.label}`}>Enter Bio:</label>
-        <input
-          type="text"
-          value={bio}
-          onChange={(event) => SetBio(event.target.value)}
-          className={`${styles.input}`}
-        />
-        <label className={`${styles.label}`}>Enter Avatar Url:</label>
-        <input
-          type="text"
-          value={avatar}
-          onChange={(event) => SetAvatar(event.target.value)}
-          className={`${styles.input}`}
-        />     
+      <div className={styles.minimize}>
+        <div className={styles.field}>
+          <label className={styles.label}>Enter Name:</label>
+          <input
+            type="text"
+            value={userProfile.name}
+            onChange={(event) => handleUserProfileChange('name', event.target.value)}
+            className={styles.input}
+          />
+          <label className={styles.label}>Enter Username:</label>
+          <input
+            type="text"
+            value={userProfile.username}
+            onChange={(event) => handleUserProfileChange('username', event.target.value)}
+            className={styles.input}
+          />
+          <label className={styles.label}>Enter Bio:</label>
+          <input
+            type="text"
+            value={userProfile.bio}
+            onChange={(event) => handleUserProfileChange('bio', event.target.value)}
+            className={styles.input}
+          />
+          <label className={styles.label}>Enter Avatar Url:</label>
+          <input
+            type="text"
+            value={userProfile.avatar}
+            onChange={(event) => handleUserProfileChange('avatar', event.target.value)}
+            className={styles.input}
+          />
+          <label className={styles.label}>Select Namespace:</label>
+         
+          <button className={styles.button} onClick={handleCreateProfile}>Create Profile</button>
+        </div>
+        {metadataUri && <p>{metadataUri}</p>}
       </div>
-    
-      <button
-        className={`${styles.button}`}
-        onClick={async (event) => {
-          event.preventDefault()
-          Profile(name,bio,username,avatar);
-          setTimeout(() => {
-            console.log("result",metadataUri)
-            // const metadataUri = result
-            create(metadataUri, selectedNamespaceOption, new PublicKey(selectedUserOption), userPublicKey);
-          }, 4000);
-        }}
-      >
-        Create Profile
-      </button>
-    </div>
-  );
+);
 };
 
-export default CreateProfile;
+export default CreateProfile;      
